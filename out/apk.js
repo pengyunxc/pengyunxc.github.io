18625af7d57d4ed35bf17f1bb6bdf08f226efe26
pengyunxc <>
2017-10-21 21:43:16 +0800

    Add apk.js

---
+++ out/apk.js
@@ -0,0 +1,5 @@
+(function($) {
+$(document).ready(function(){  
+    $("#insert").html("<table class=\"solid widthstyle\"><tr><td><ul><li><span>安卓应用</span></li></ul><ol><li><a target=\"_blank\" href=\"https://pan.baidu.com/s/1nvbv9e5\">驾考内部助考版.APK（密码: z3et）</a> <br /><a class=\"graycolor\" target=\"_blank\" href=\"https://www.jianguoyun.com/p/DQ1e8LEQoajLBhig6Tc\">-->备用下载（密码: E4KAom）</a></li></ol></td></tr></table>");
+});
+})(window.jQuery);
