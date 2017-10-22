a82507515a59414cac1a9c843438471610d3469b
pengyunxc <>
2017-10-22 20:41:29 +0800

    add ga1026 to download page.

--- out/apk.js
+++ out/apk.js
@@ -1,5 +1,8 @@
 (function($) {
 $(document).ready(function(){  
-    $("#insert").html("<table class=\"solid widthstyle\"><tr><td><ul><li><span>安卓应用</span></li></ul><ol><li><a target=\"_blank\" href=\"https://pan.baidu.com/s/1nvbv9e5\">驾考内部助考版.APK（密码: z3et）</a> <br /><a class=\"graycolor\" target=\"_blank\" href=\"https://www.jianguoyun.com/p/DQ1e8LEQoajLBhig6Tc\">-->备用下载（密码: E4KAom）</a></li></ol></td></tr></table>");
-});
+    $("#insert").html("<table class=\"solid widthstyle\"><tr><td><ul><li><span>安卓应用</span></li></ul><ol><li><a target=\"_blank\" href=\"https://pan.baidu.com/s/1nvbv9e5\">驾考内部助考版.APK（密码: z3et）</a> <br /><a class=\"graycolor\" target=\"_blank\" href=\"https://www.jianguoyun.com/p/DQ1e8LEQoajLBhig6Tc\">-->备用下载（密码: E4KAom）</a></li></ol></td></tr></table>"
+                        +"<br />"
+                        +"<table class=\"solid widthstyle\"><tr><td><ul><li><span>新规</span></li></ul><ol><li><a target=\"_blank\" href=\"https://pan.baidu.com/s/1i5IhgK9\">PDF GA1026-2017（密码: s793）</a> <br /></li><li><a target=\"_blank\" href=\"https://pan.baidu.com/s/1dF0E0P7\">PDF GA1026-2017（密码: pv5t）</a> <br /></li></ol></td></tr></table>""
+    );
+-);
 })(window.jQuery);
