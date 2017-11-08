43446df6756ea0443795ed8d5f9af5aded5618a8
pengyunxc <>
2017-11-09 06:11:55 +0800

    Update apk.js

--- out/apk.js
+++ out/apk.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
     $("#insert").html("<table class=\"solid widthstyle\"><tr><td><ul><li><span>安卓应用</span></li></ul><ol><li><a target=\"_blank\" href=\"https://pan.baidu.com/s/1nvbv9e5\">驾考内部助考版.APK（密码: z3et）</a> <br /><a class=\"graycolor\" target=\"_blank\" href=\"https://www.jianguoyun.com/p/DQ1e8LEQoajLBhig6Tc\">-->备用下载（密码: E4KAom）</a></li></ol></td></tr></table>"
                         +"<br />"
                         +"<table class=\"solid widthstyle\"><tr><td><ul><li><span>新规</span></li></ul><ol><li><a target=\"_blank\" href=\"https://pan.baidu.com/s/1i5IhgK9\">PDF GA1026-2017（密码: s793）</a> <br /></li><li><a target=\"_blank\" href=\"https://pan.baidu.com/s/1dF0E0P7\">PDF GA1026-2012（密码: pv5t）</a> <br /></li></ol></td></tr></table>"
+                        +"<br />"
+                        +"<table class=\"solid widthstyle\"><tr><td><ul><li><span>资料</span></li></ul><ol><li><a target=\"_blank\" href=\"https://pan.baidu.com/s/1cnouiq\">2018年鹏云学车科目一最新VIP题库.pdf（密码: ykcr）</a> <br /><a class=\"graycolor\" target=\"_blank\" href=\"http://www.pengyunxc.com/files/doc/2018%E5%B9%B4%E9%B9%8F%E4%BA%91%E5%AD%A6%E8%BD%A6%E7%A7%91%E7%9B%AE%E4%B8%80%E6%9C%80%E6%96%B0VIP%E9%A2%98%E5%BA%93.pdf\">-->备用下载(3M)</a></li><li><a target=\"_blank\" href=\"https://pan.baidu.com/s/1o8MHGYQ\">2017驾照考试科目一知识点归纳总结（密码: ty4b）</a> <br /><a class=\"graycolor\" target=\"_blank\" href=\"http://pengyunxc.github.io/out/2017驾照考试科目一知识点归纳总结.pdf\">-->备用下载(9.6M)</a></li></ol></td></tr></table>"
     );
 });
 })(window.jQuery);
